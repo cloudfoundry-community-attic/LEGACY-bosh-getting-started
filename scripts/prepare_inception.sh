9faf1a587a30a28211b28cee74bdd504f9d89d65
Dr Nic Williams <drnicwilliams@gmail.com>
2012-07-05 23:21:58 -0700

    prepare_inception script is idempotent; is run as root

--- scripts/prepare_inception.sh
+++ scripts/prepare_inception.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ $EUID -eq 0 ]]; then
-   echo "ERROR: This script must not be run as root" 1>&2
-   exit 1
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: This script must be run as root" 1>&2
+  exit 1
 fi
-orig_user=$(whoami)
+
+orig_user=$1
+if [[ -z $orig_user ]]; then
+  echo "WARNING: Can pass non-root username to copy authorized_keys and .bashrc to vcap user"
+fi
 
 groupadd vcap 
 useradd vcap -m -g vcap
-mkdir /home/vcap/.ssh
+mkdir -p /home/vcap/.ssh
 chown -R vcap:vcap /home/vcap/.ssh
-cp /home/${orig_user}/.ssh/authorized_keys /home/vcap/.ssh/authorized_keys
-cp /home/${orig_user}/.bashrc /home/vcap/
 
 su -c "ssh-keygen -f ~/.ssh/id_rsa -N ''" vcap
 
 bosh_app_dir=/var/vcap
 mkdir -p ${bosh_app_dir}
-cp /home/${orig_user}/.ssh/authorized_keys ${bosh_app_dir}/
 mkdir -p ${bosh_app_dir}/bosh
 export PATH=${bosh_app_dir}/bosh/bin:$PATH
 mkdir -p ${bosh_app_dir}/deploy ${bosh_app_dir}/storage
@@ -25,20 +26,31 @@ chown vcap:vcap ${bosh_app_dir}/deploy ${bosh_app_dir}/storage
 echo "export PATH=${bosh_app_dir}/bosh/bin:\$PATH" >> /root/.bashrc
 echo "export PATH=${bosh_app_dir}/bosh/bin:\$PATH" >> /home/vcap/.bashrc
 
+if [[ -n $orig_user ]]; then
+  cp /home/${orig_user}/.ssh/authorized_keys ${bosh_app_dir}/
+  cp /home/${orig_user}/.ssh/authorized_keys /home/vcap/.ssh/authorized_keys
+  cp /home/${orig_user}/.bashrc /home/vcap/
+fi
+
 apt-get update
 apt-get install build-essential libsqlite3-dev curl rsync git-core \
 libmysqlclient-dev libxml2-dev libxslt-dev libpq-dev libsqlite3-dev genisoimage -y
 
 echo "install: --no-ri --no-rdoc" > /etc/gemrc
 echo "update: --no-ri --no-rdoc" > /etc/gemrc
-curl -L get.rvm.io | bash -s stable
-source /etc/profile
+if [[ -x rvm ]]
+then
+  rvm get stable
+else
+  curl -L get.rvm.io | bash -s stable
+  source /etc/profile
+fi
 rvm install 1.9.3 # oh god this takes a long time
 rvm 1.9.3
 rvm alias create default 1.9.3
 
 
-mkdir /var/vcap/bootstrap
+mkdir -p /var/vcap/bootstrap
 cd /var/vcap/bootstrap
 git clone https://github.com/cloudfoundry/bosh.git
 
@@ -61,4 +73,6 @@ cd /var/vcap/bootstrap/bosh/aws_registry/
 bundle install --without=development test
 bundle exec rake install
 
+echo "Sanity check..."
+echo "$ bosh help micro"
 bosh help micro
